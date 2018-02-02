f48ffaca653a3cd76bd55b9c9b8981b2e5bad3f6
pattumehala <35220817+pattumehala@users.noreply.github.com>
2018-02-02 15:41:12 +0530

    primeinterval.C
    prime number between two interval

--- primeinterval.C
+++ primeinterval.C
@@ -4,21 +4,21 @@ int main()
 int low,high,count,i;
 printf("Enter the two numbwrs(intervals):");
 scanf("%d%d",&low,&high);
-printf("\nPrime number between %d and %d are:",low,high);
+printf("Prime number between %d and %d are:",low,high);
 while(low<high)
 {
 count=0;
-for(i=2;i<low/2;++i)
+for(i=2;i<=low/2;++i)
 {
 if(low%i==0)
+{
 count=1;
 break;
 }
 }
 if(count==0)
-{
 printf("%d",low);
-}
 ++low;
+}
 return 0;
 }
